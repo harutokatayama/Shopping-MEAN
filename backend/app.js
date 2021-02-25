df5ca94c089da923c3d7cc30401bda03bccc1006
harutokatayama <greva8610@yahoo.co.jp>
2021-02-26 00:51:22 +0900

    Edit backend app.js

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,32 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
-const multer = require('multer');
-
-const errorController = require('./controllers/error');
-
-// 後で変更
-const MONGODB_URI = 
-  'mongodb+srv://FMKtrHE4emge6xmT:FMKtrHE4emge6xmT@cluster0.r7bof.mongodb.net/shop';
 
 const app = express();
 
-const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
-});
-const csrfProtection = csrf();
+mongoose.connect('mongodb+srv://Haruto:FM4dwHBEThulA52N@shop-mean.ofec2.mongodb.net/myFirstDatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        "*"
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-    );
-    next();
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
 });
 
 module.exports = app;
