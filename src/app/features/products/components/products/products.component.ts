7a467ab6a47fffc355a6f407020634499858ffec
harutokatayama <greva8610@yahoo.co.jp>
2021-03-04 23:46:44 +0900

    Edit products

--- src/app/features/products/components/products/products.component.ts
+++ src/app/features/products/components/products/products.component.ts
@@ -1,21 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from 'src/app/features/auth/services/auth.service';
+import { Product } from '../../product.model';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
   products = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,];
+  // products: Product[] = [];
   productCode = 'ZLFfW5Vh2Y28NQRu';
+  isLoading = false;
   totalProducts = 10;
   productsPerPage = 2;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
+  userIsAuthenticated = false;
+  private productsSub: Subscription;
+  private authStatusSub: Subscription;
 
-  constructor() { }
+  constructor(
+    public productsService: ProductsService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.productsService.getProducts(this.productsPerPage, this.currentPage);
+    this.productsSub = this.productsService.getProductUpdatedListener()
+      .subscribe((productData: { products: Product[], productCount: number }) => {
+        this.isLoading = false;
+        this.totalProducts = productData.productCount;
+        // this.products = productData.products;
+      });
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
+  }
+
+  onChangePage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.productsPerPage = pageData.pageSize;
+    this.productsService.getProducts(this.productsPerPage, this.currentPage);
+  }
+
+  ngOnDestroy() {
+    this.productsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
   }
 
 }
--- src/app/features/products/services/products.service.ts
+++ src/app/features/products/services/products.service.ts
@@ -1,14 +1,78 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
+
+import { Product } from "../product.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductsService {
-    getProducts() {
+    private products: Product[] = [];
+    private productsUpdated = new Subject<{ products: Product[]; productCount: number }>();
 
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) {}
+
+    getProducts(productsPerPage: number, currentPage: number) {
+        const queryParams = `?pagesize=${productsPerPage}&page=${currentPage}`;
+        this.http
+          .get<{ message: string; products: any; maxProducts: number }>(
+            'http://localhost:3000/api/products/' + queryParams
+          )
+          .pipe(
+              map(productData => {
+                  return {
+                      products: productData.products.map(product => {
+                          return {
+                            id: product._id,
+                            title: product.title,
+                            price: product.price,
+                            imagePath: product.imagePath,
+                            description: product.description,
+                            quantity: product.quantity,
+                            weight: product.weight,
+                            category: product.category,
+                            country: product.country,
+                            height: product.height,
+                            width: product.width
+                          };
+                      }),
+                      maxProducts: productData.maxProducts
+                  };
+              })
+          )
+          .subscribe(transformedProductData => {
+              this.products = transformedProductData.products;
+              this.productsUpdated.next({
+                  products: [...this.products],
+                  productCount: transformedProductData.maxProducts
+              });
+          });
+    }
+
+    getProductUpdatedListener() {
+        return this.productsUpdated.asObservable();
     }
     
-    getProduct() {
-        
+    getProduct(id: string) {
+        return this.http.get<{
+            _id: string;
+            title: string;
+            price: number;
+            imagePath: string;
+            description: string;
+            quantity: number;
+            weight: number;
+            category: string;
+            country: string;
+            height: number;
+            width: number;
+        }>('http://localhost:3000/api/products/' + id);
     }
+
 }
