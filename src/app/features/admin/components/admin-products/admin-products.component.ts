759510950255e5f04ad358a668b5fc0a124fdc2b
harutokatayama <greva8610@yahoo.co.jp>
2021-03-04 23:45:40 +0900

    Edit admin-products

--- src/app/features/admin/components/admin-products/admin-products.component.ts
+++ src/app/features/admin/components/admin-products/admin-products.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Subscription } from 'rxjs';
 
+import { Product } from 'src/app/features/products/product.model';
 import { AdminProductsService } from '../../services/admin-products.service';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -10,32 +13,52 @@ import { AdminProductsService } from '../../services/admin-products.service';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
   products = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,];
+  // products: Product[] = [];
   productCode = 'ZLFfW5Vh2Y28NQRu';
-  isLoadingMode = true;
   isLoading = false;
   totalProducts = 10;
   productsPerPage = 2;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
   userIsAuthenticated = false;
-  userId: string;
   private productsSub: Subscription;
   private authStatusSub: Subscription;
 
-  constructor(private adminProductsService: AdminProductsService) { }
+  constructor(
+    private adminProductsService: AdminProductsService,
+    private adminService: AdminService
+  ) { }
   
   ngOnInit(): void {
     this.isLoading = true;
     // this.adminProductsService.getProducts(this.productsPerPage, this.currentPage);
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 0)
+    this.productsSub = this.adminProductsService.getProductUpdateListener()
+      .subscribe((productData: { products: Product[], productCount: number }) => {
+        this.isLoading = false;
+        this.totalProducts = productData.productCount;
+        // this.products = productData.products;
+      });
+    this.userIsAuthenticated = this.adminService.getIsAuth();
+    this.authStatusSub = this.adminService
+      .getAuthenticatedListener()
+      .subscribe(iaAuthenticated => {
+        this.userIsAuthenticated = this.userIsAuthenticated
+      });
   }
 
-  onDelete(postId: string) {
+  onChangedPage(pageData: PageEvent) {
     this.isLoading = true;
-    this.adminProductsService.deleteProduct(postId).subscribe(() => {
+    this.currentPage = pageData.pageIndex + 1;
+    this.productsPerPage = pageData.pageSize;
+    this.adminProductsService.getProducts(this.productsPerPage, this.currentPage)
+  }
+
+  onDelete(productId: string) {
+    this.isLoading = true;
+    this.adminProductsService.deleteProduct(productId).subscribe(() => {
       this.adminProductsService.getProducts(this.productsPerPage, this.currentPage);
+    }, () => {
+      this.isLoading = false;
     });
   }
 
