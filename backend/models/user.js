0ec0587f1a26b780922a10e56766919f9257f4d4
harutokatayama <greva8610@yahoo.co.jp>
2021-03-04 23:44:34 +0900

    Edit user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,63 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+const Schema = mongoose.Schema;
+
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true
+        },
+        quantity: { type: Number, required: true }
+      }
+    ]
+  }
 });
 
 userSchema.plugin(uniqueValidator);
+
+userSchema.methods.addToCart = product => {
+  console.log(product);
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCartItems;
+  return this.save();
+}
+
+userSchema.methods.removeFromCart = productId => {
+  console.log(productId);
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+}
+
+userSchema.methods.clearCart = () => {
+  this.cart = { items: [] };
+  return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
